5ae503d213a4c2ea6a93173da8d36ad169dbe012
Programming Hero <programming.hero1@gmail.com>
2025-11-20 18:21:24 -0600

    created be a rider page

--- src/pages/Rider/Rider.jsx
+++ src/pages/Rider/Rider.jsx
@@ -1,9 +1,121 @@
 import React from 'react';
+import { useForm, useWatch } from 'react-hook-form';
+import useAuth from '../../hooks/useAuth';
+import useAxiosSecure from '../../hooks/useAxiosSecure';
+import { useLoaderData } from 'react-router';
 
 const Rider = () => {
+    const {
+        register,
+        handleSubmit,
+        control,
+        // formState: { errors } 
+    } = useForm();
+    const { user } = useAuth();
+    const axiosSecure = useAxiosSecure();
+
+    const serviceCenters = useLoaderData();
+    const regionsDuplicate = serviceCenters.map(c => c.region);
+
+    const regions = [...new Set(regionsDuplicate)];
+    // explore useMemo useCallback
+    const districtsByRegion = (region) => {
+        const regionDistricts = serviceCenters.filter(c => c.region === region);
+        const districts = regionDistricts.map(d => d.district);
+        return districts;
+    }
+
+    const senderRegion = useWatch({ control, name: 'senderRegion' });
+
+    const handleRiderApplication = data => {
+        console.log(data);
+    }
     return (
         <div>
-            
+            <h2 className="text-4xl text-primary">Be a Rider</h2>
+            <form onSubmit={handleSubmit(handleRiderApplication)} className='mt-12 p-4 text-black'>
+                
+                {/* two column */}
+                <div className='grid grid-cols-1 md:grid-cols-2 gap-12'>
+                    {/* sender Details */}
+
+                    <fieldset className="fieldset">
+                        <h4 className="text-2xl font-semibold">Rider Details</h4>
+                        {/* sender name */}
+                        <label className="label">Sender Name</label>
+                        <input type="text" {...register('senderName')}
+                            defaultValue={user?.displayName}
+                            className="input w-full" placeholder="Sender Name" />
+
+                        {/* sender email */}
+                        <label className="label">Sender Email</label>
+                        <input type="text" {...register('senderEmail')}
+                            defaultValue={user?.email}
+                            className="input w-full" placeholder="Sender Email" />
+
+                        {/* sender region */}
+                        <fieldset className="fieldset">
+                            <legend className="fieldset-legend">Sender Regions</legend>
+                            <select {...register('senderRegion')} defaultValue="Pick a region" className="select">
+                                <option disabled={true}>Pick a region</option>
+                                {
+                                    regions.map((r, i) => <option key={i} value={r}>{r}</option>)
+                                }
+                            </select>
+                        </fieldset>
+
+                        {/* sender districts */}
+                        <fieldset className="fieldset">
+                            <legend className="fieldset-legend">Sender Districts</legend>
+                            <select {...register('senderDistrict')} defaultValue="Pick a district" className="select">
+                                <option disabled={true}>Pick a district</option>
+                                {
+                                    districtsByRegion(senderRegion).map((r, i) => <option key={i} value={r}>{r}</option>)
+                                }
+                            </select>
+                        </fieldset>
+
+
+                        {/* sender address */}
+                        <label className="label mt-4">Sender Address</label>
+                        <input type="text" {...register('senderAddress')} className="input w-full" placeholder="Sender Address" />
+
+
+                    </fieldset>
+                    {/* receiver Details */}
+                    <fieldset className="fieldset">
+                        <h4 className="text-2xl font-semibold">Receiver Details</h4>
+                        {/* receiver name */}
+                        <label className="label">Receiver Name</label>
+                        <input type="text" {...register('receiverName')} className="input w-full" placeholder="Receiver Name" />
+
+                        {/* receiver email */}
+                        <label className="label">Receiver Email</label>
+                        <input type="text" {...register('receiverEmail')} className="input w-full" placeholder="Receiver Email" />
+
+                        {/* receiver region */}
+                        <fieldset className="fieldset">
+                            <legend className="fieldset-legend">Receiver Regions</legend>
+                            <select {...register('receiverRegion')} defaultValue="Pick a region" className="select">
+                                <option disabled={true}>Pick a region</option>
+                                {
+                                    regions.map((r, i) => <option key={i} value={r}>{r}</option>)
+                                }
+                            </select>
+                        </fieldset>
+
+                        
+
+
+                        {/* receiver address */}
+                        <label className="label mt-4">Receiver Address</label>
+                        <input type="text" {...register('receiverAddress')} className="input w-full" placeholder="Receiver Address" />
+
+
+                    </fieldset>
+                </div>
+                <input type="submit" className='btn btn-primary mt-8 text-black' value="Send Parcel" />
+            </form>
         </div>
     );
 };
