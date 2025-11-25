fa0cd1fda138e83e58c9bfa69d4a07e3da0e0d19
Programming Hero <programming.hero1@gmail.com>
2025-11-25 09:14:18 -0600

    display tracking logs

--- src/pages/ParcelTrack/ParcelTrack.jsx
+++ src/pages/ParcelTrack/ParcelTrack.jsx
@@ -22,8 +22,10 @@ const ParcelTrack = () => {
 
             <ul className="timeline timeline-vertical">
                 {
-                    trackings.map(log => <li>
-                    <div className="timeline-start">1984</div>
+                    trackings.map(log => <li key={log._id}>
+                    <div className="timeline-start">
+                        {new Date(log.createdAt).toLocaleString()}
+                    </div>
                     <div className="timeline-middle">
                         <svg
                             xmlns="http://www.w3.org/2000/svg"
@@ -38,7 +40,9 @@ const ParcelTrack = () => {
                             />
                         </svg>
                     </div>
-                    <div className="timeline-end timeline-box">First Macintosh computer</div>
+                    <div className="timeline-end timeline-box">
+                        <span className='text-xl'>{log.details}</span>
+                    </div>
                     <hr />
                 </li>)
                 }
